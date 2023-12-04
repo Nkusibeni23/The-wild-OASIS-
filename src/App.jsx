bc5232f77469a34b172c87a72c1e6cd551d74589
Benny Chrispin <ndizibaidu23@gmail.com>
2023-12-04 19:18:26 +0200

    Styled component Props and the css Function

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
+import Heading from "./ui/Heading";
 
 const StyledApp = styled.main`
   padding: 0;
@@ -21,10 +16,11 @@ export default function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>The Wild Oasis</H1>
+        <Heading as="h1">The Wild Oasis</Heading>
+        <Heading as="h2">Check In and Check Out</Heading>
         <Button onClick={() => alert("check in")}>Check In</Button>
         <Button onClick={() => alert("check out")}>Check Out</Button>
-
+        <Heading as="h3">Form</Heading>
         <Input type="number" placeholder="number of quests" />
         <Input type="number" placeholder="number of guests" />
       </StyledApp>
---
+++ src/ui/Heading.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+// const test = `text-align: center`;
+
+const Heading = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+  text-align: center;
+`;
+
+export default Heading;
